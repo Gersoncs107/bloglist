81423201c65d816f861ba7c5bcc1f8e9a23e432b
Gersoncs107 <gerson.silva107@gmail.com>
2025-09-13 22:28:13 -0300

    bloglist-update

--- models/blog.js
+++ models/blog.js
@@ -14,3 +14,20 @@ mongoose.connect(url)
   .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
   })
+
+  const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+  })
+    blogSchema.set('toJSON', {
+        transform: (document, returnedObject) => {
+            returnedObject.id = returnedObject._id.toString()
+            delete returnedObject._id
+            delete returnedObject.__v
+        }
+    })
+
+  module.exports = mongoose.model('Blog', blogSchema) 
+  
