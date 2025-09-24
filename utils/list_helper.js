ec21a52e263ace6ca9426f4ff81adad3200f955d
Gersoncs107 <gerson.silva107@gmail.com>
2025-09-23 23:16:00 -0300

    bloglist-update

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -9,6 +9,10 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  
+}
+
 module.exports = {
   dummy,
   totalLikes
