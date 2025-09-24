da1ad2677603e3f319a7884ac77e371e0c0df8a6
Gersoncs107 <gerson.silva107@gmail.com>
2025-09-23 22:31:56 -0300

    bloglist-update

--- tests/totalLikes .test.js
+++ tests/totalLikes .test.js
@@ -76,4 +76,14 @@ describe('total likes', () => {
         const result = list_helper.totalLikes(listWithNoBlogs)
         expect(result).toBe(0)
     })
+
+    test('when list has only one blog, equals the likes of that', () => {
+      const result = list_helper.totalLikes(listWithOneBlog)
+      expect(result).toBe(5)
+    })
+
+    test('of a bigger list is calculated right', () => {
+      const result = list_helper.totalLikes(listWithManyBlogs)
+      expect(result).toBe(36)
+    })
 })
