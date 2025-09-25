8ccf998d7970972a9882c10286e86567ea6a26f9
Gersoncs107 <gerson.silva107@gmail.com>
2025-09-24 22:50:37 -0300

    bloglist-update

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -10,10 +10,13 @@ const totalLikes = (blogs) => {
 }
 
 const favoriteBlog = (blogs) => {
-  if (blogs.length === 0) {
-    return null
+  if (blogs.length === 0) return null
+  const favorite = blogs.reduce((prev, current) => (prev.likes > current.likes ? prev : current))
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes
   }
-  return blogs.reduce((fav, blog) => (blog.likes > fav.likes ? blog : fav), blogs[0])
 }
 
 module.exports = {
