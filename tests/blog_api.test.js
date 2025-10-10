dd40d5f17c09132467f9dbd289d12fcb2df57b8f
Gersoncs107 <gerson.silva107@gmail.com>
2025-10-09 22:13:37 -0300

    bloglist-update

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,22 +1,42 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
+const Blog = require('../models/blog'); // Certifique-se que o modelo está correto
 
 const api = supertest(app);
+
+const initialBlogs = [
+  {
+    title: 'Primeiro blog',
+    author: 'Autor 1',
+    url: 'http://primeiro.com',
+    likes: 1
+  },
+  {
+    title: 'Segundo blog',
+    author: 'Autor 2',
+    url: 'http://segundo.com',
+    likes: 2
+  }
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  await Blog.insertMany(initialBlogs);
+});
 
 test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
-}, 20000) // 20 seconds timeout
-
-afterAll(async () => {
-  await mongoose.connection.close()
-})
+}, 20000);
 
 test('there are two blogs', async () => {
-  const response = await api.get('/api/blogs')
+  const response = await api.get('/api/blogs');
+  expect(response.body).toHaveLength(2);
+});
 
-  expect(response.body).toHaveLength(2)
-})
+afterAll(async () => {
+  await mongoose.connection.close();
+});
