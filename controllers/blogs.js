7535bcd8e6309f8740e5f998d834d04b1b03535e
Gersoncs107 <gerson.silva107@gmail.com>
2025-10-26 22:11:46 -0300

    bloglist-update

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,5 +1,6 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
@@ -17,6 +18,8 @@ blogsRouter.get('/:id', async (request, response, next) => {
 
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
+  const user = await User.findById(body.userId)
+
 
   if(!body.title || !body.url){
       return response.status(400).json({ error: 'title or url missing' })
@@ -27,9 +30,12 @@ blogsRouter.post('/', async (request, response, next) => {
       author: body.author,
       url: body.url,
       likes: body.likes || 0,
+      user: user.id
   })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
   response.status(201).json(savedBlog)
 })
 
