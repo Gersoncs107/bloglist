277f55ed2cc0252c116c0446413b2c69a36321d3
Gersoncs107 <gerson.silva107@gmail.com>
2025-10-28 22:34:47 -0300

    bloglist-update

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const usersRouter = require('./controllers/users')
 const mongoose = require('mongoose')
+const loginRouter = require('./controllers/login')
 
 mongoose.set('strictQuery', false)
 
@@ -25,6 +26,7 @@ app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.get('/', (req, res) => {
   res.send('<h1>Blog Working!</h1>')
