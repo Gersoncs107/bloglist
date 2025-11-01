ed9fcc12ad21737063cd191e99193aa1158cb450
Gersoncs107 <gerson.silva107@gmail.com>
2025-11-01 19:20:02 -0300

    bloglist-update

--- models/user.js
+++ models/user.js
@@ -3,11 +3,16 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true,
-    unique: true
+    required: [true, 'username is required'],
+    unique: true,
+    minlength: [3, 'username must be at least 3 characters long']
   },
   name: String,
-  passwordHash: String,
+  passwordHash: {
+    type: String,
+    required: true,
+    minlength: [3, 'password must be at least 3 characters long']
+  },
   blogs: [
     {
       type: mongoose.Schema.Types.ObjectId,
@@ -21,11 +26,8 @@ userSchema.set('toJSON', {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
-    // the passwordHash should not be revealed
     delete returnedObject.passwordHash
   }
 })
 
-const User = mongoose.model('User', userSchema)
-
-module.exports = User
+module.exports = mongoose.model('User', userSchema)
