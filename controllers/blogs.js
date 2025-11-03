320db3aaf70e62e54c5beb6d59dc1400451ffa07
Gersoncs107 <gerson.silva107@gmail.com>
2025-11-02 22:14:14 -0300

    bloglist-update

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -13,7 +13,11 @@ const getTokenFrom = request => {
 }
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', {
+    username: 1,
+    name: 1,
+    id: 1
+  })
   response.json(blogs)
 })
 
