d385903db83fd4fb2c2d3ccc6e333edb893c0a4a
Gersoncs107 <gerson.silva107@gmail.com>
2025-11-06 19:05:32 -0300

    bloglist-update

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,3 @@
-// controllers/users.js
 const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
@@ -16,21 +15,21 @@ usersRouter.get('/', async (request, response) => {
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
-  // 1. Verificar se username e password foram fornecidos
+  
   if (!username || !password) {
     return response.status(400).json({
       error: 'username and password are required'
     })
   }
 
-  // 2. Verificar comprimento mínimo
+ 
   if (username.length < 3 || password.length < 3) {
     return response.status(400).json({
       error: 'username and password must be at least 3 characters long'
     })
   }
 
-  // 3. Verificar se username já existe
+ 
   const existingUser = await User.findOne({ username })
   if (existingUser) {
     return response.status(400).json({
@@ -38,11 +37,11 @@ usersRouter.post('/', async (request, response) => {
     })
   }
 
-  // 4. Hash da senha
+  
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
 
-  // 5. Criar usuário
+  
   const user = new User({
     username,
     name,
